8e64dc3c6ff8c15e7714abfd33672f8f5b78d7f6
Ruben David Parga Garcia <rparga@unitedvirtualities.com>
2020-02-18 11:05:12 -0700

    step 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
-const http = require('http');
+const express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+let users = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-server.listen(3000, () => {
-  console.log('Server is running...');
+//GET /users
+app.get('/users', (req, res) => res.json(users));
+
+
+//GET /user/:id
+app.get('/user/:id', (req, res) => {
+    let user = users.find(user => user.id == req.params.id);
+    res.json(user);
 });
+
+//POST id{0}
+app.post('/user', (req, res) => {    
+    users.push({ id: 0 });    
+    res.json(users);
+});
+
+//DELETE /user/:id
+app.delete('/user/:id', (req, res) => {
+
+   
+    if(users.length > 0){
+        users = [];
+        res.status(202).json({
+            ok: true
+        })
+    } else {
+        res.status(204).json({
+            ok: true
+        })
+    }
+
+});
+
+//Port
+app.listen(3000, () => {
+    console.log('Listen on port 3000');
+})
